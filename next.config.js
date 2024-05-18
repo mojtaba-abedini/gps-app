2251571b06c0727a42c9ec45e7afd16858d8ed34
unknown <mojtaba.abedini1366@gmail.com>
2024-05-18 18:24:43 +0330

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
 
-
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: false
-})
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: false,
-    
-    publicRuntimeConfig: {
-        // Will be available on both server and client
-        backendUrl: process.env.BACKEND_URL,
-    },
-    swcMinify: true,
-    distDir: "build",
-}
-
-module.exports = withPWA(nextConfig)
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+  });
+  
+  module.exports = withPWA({
+    // Your Next.js config
+  });
