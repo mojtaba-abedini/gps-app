38e597537af990bfd190f20e3aba86b8fafb3eb1
unknown <mojtaba.abedini1366@gmail.com>
2024-06-30 19:49:36 +0330

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 
   
-  module.exports = ({
-    output : 'export',
-    trailingSlash: true
-  });
+  // module.exports = ({
+  //   output : 'export',
+  //   trailingSlash: true
+  // });
+
+
+
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+module.exports = withPWA({
+  // output : 'export',
+  // trailingSlash: true
+});
---
+++ src/app/head.js
@@ -0,0 +1,14 @@
+// app/head.js
+export default function Head() {
+    return (
+      <>
+        <title>اپلیکیشن ایز اسمارت</title>
+        <meta name="description" content="یک اپلیکیشن در زمینه مدیریت ردیاب خودرو" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icon-192x192.png" />
+        <meta name="theme-color" content="#000000" />
+      </>
+    );
+  }
+  
